f9420a669e2ffc4140706dec9ae2c6b2ed4899a0
Patrick J. Wu <wotingwu@live.com>
2018-04-04 18:28:25 -0500

    b+-tree basic implementation

--- b+-tree.cpp
+++ b+-tree.cpp
@@ -1,13 +1,48 @@
-#include <iostream>
-using namespace std;
+#include "b+-tree.h"
 
-struct bpt_node{
-	int *data;
-	int n;
-	bool leaf;
-	bpt_node *root_node;
-	bpt_node *next_node;
-	bpt_node *prev_node;
+bpt_node::bpt_node(){
+	num=NULL;
+	leaf=NULL;
+	root_node=NULL;
+	next_node=NULL;
+	prev_node=NULL;
 }
 
+bpt_node::bpt_node(bool l, int d, bpt_node *f, bpt_node *n, bpt_node *p){
+	leaf=l;
+	num=d;
+	root_node=f;
+	next_node=n;
+	prev_node=p;
+}
 
+bpt_node::~bpt_node(){
+	num=NULL;
+	leaf=NULL;
+	root_node=NULL;
+	next_node=NULL;
+	prev_node=NULL;
+	delete root_node;
+	delete prev_node;
+	delete next_node;
+}
+
+bpt_node bpt_node::GetNext(){
+	return *next_node;
+}
+
+bpt_node bpt_node::GetPrev(){
+	return *prev_node;
+}
+
+bpt_node bpt_node::GetRoot(){
+	return *root_node;
+}
+
+bool bpt_node::IsLeaf(){
+	return leaf;
+}
+
+int bpt_node::GetValue(){
+	return num;
+}
--- b+-tree.h
+++ b+-tree.h
@@ -1,2 +1,30 @@
 #include <iostream>
+using namespace std;
+
+class bpt_node
+{
+	private:
+		int num;
+		bool leaf;
+		bpt_node *root_node;
+		bpt_node *next_node;
+		bpt_node *prev_node;
+	public:
+		bpt_node();
+		bpt_node(bool l, int d, bpt_node *f, bpt_node *n, bpt_node *p);
+		~bpt_node();
+
+		bpt_node GetNext();
+		bpt_node GetPrev();
+		bpt_node GetRoot();
+		bool IsLeaf();
+		int GetValue();
+
+		bpt_node search(int d);
+		bpt_node ins(int d);
+		bpt_node del(int d);
+		void print(bpt_node top_node);
+}
+
+
 
